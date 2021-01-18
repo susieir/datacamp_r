8ebb2ee7cfe69053faccc29fa2ab643a20bc8ecc
Susie <susie@irons.email>
2021-01-18 15:46:19 +0000

    Intermediate R progress

--- Intermediate_r.R
+++ Intermediate_r.R
@@ -232,3 +232,163 @@ search()
 
 # Require - also loads packages
 # If package not installed, returns warning rather than error
+
+# Chapter 4 - The apply family
+# A: lapply
+
+nyc <- list(pop = 8405837,
+            boroughs = c("Manhattan", "Bronx", "Brooklyn",
+                         "Queens", "Staten Island"),
+            capital = FALSE)
+
+# Loop to print data types of nyc data
+for(info in nyc) {
+  print(class(info))
+}
+
+# Quicker alternative
+lapply(nyc, class)
+
+# Cities 
+
+cities <- c("New York", "Paris", "London", "Tokyo",
+            "Rio de Janeiro", "Cape Town")
+
+num_chars <- c()  # Create empty vector
+for(i in 1:length(cities)) {
+  num_chars[i] <- nchar(cities[i])  # Fill up vectors
+}
+num_chars
+
+lapply(cities, nchar)  
+
+# Always returns a list, irrespective of data structure
+# If want to turn list into a vector
+unlist(lapply(cities, nchar))
+
+# lapply can be used with own functions
+
+oil_prices <- list(2.37, 2.49, 2.18,
+                   2.22, 2.47, 2.32)
+
+multiply <- function(x, factor) ~{
+  3 * factor
+}
+
+result <-
+  lapply(oil_prices, multiply, factor=3)
+
+str(result)
+unlist(result)
+
+# Anonymous function - function without a name
+# These can be used within lapply
+lapply(list(1,2,3), function(x) {3 * x})
+
+# B: sapply
+# lapply()
+#  - Apply function over list or vector
+#  - Function can return R objects of different classes
+#  - List necessary to store heterogeneous content
+#  - However, often homogeneous content
+
+# sapply - where all results have the same type
+sapply(cities, nchar)  
+# calls lapply and then uses simply to array to convert to array/vector
+# Also names vector
+# can choose not to use names...
+sapply(cities, nchar, USE.NAMES = FALSE)
+
+# First and list - splits str to letters and obtains min and max letter
+first_and_last <- function(name) {
+  name <- gsub(" ", "", name)
+  letters <- strsplit(name, split = "")[[1]]
+  c(first = min(letters), last=max(letters))
+}
+
+first_and_last("New York")
+
+sapply(cities, first_and_last)  # Returns matrix
+
+unique_letters <- function(name) {
+  name <- gsub(" ", "", name)
+  letters <- strsplit(name, split = "")[[1]]
+  unique(letters)
+}
+
+unique_letters("London")
+
+lapply(cities, unique_letters)  # List of vectors
+# But different number of letters
+sapply(cities, unique_letters)  
+# Same result as lapply, because couldn't be simplified
+# Note - result is still a list!
+
+# C: vapply
+# Recap
+# lapply()
+#  - Apply a fun over a list of vector
+#  - Output = list
+
+# sapply()
+#  - Apply function over list or vector
+#  - try to simplify list to array
+
+# vapply()
+#  - Apply function over list or vector
+#  - Similar to sapply - explicitly specificy output format
+
+sapply(cities, nchar)
+vapply(cities, nchar, numeric(1))  # Numeric vector len 1
+
+# vapply(x, FUN, FUN.VALUE, ..., USE.NAMES = TRUE)
+# FUN.VALUE - general template for return value of fun
+
+# vapply - safer alternative to sapply
+
+sapply(cities, first_and_last)
+vapply(cities, first_and_last, character(2))  # Returns char vector len 2
+# Incorrect FUN.VALUE - generates error
+
+# Chapter 5 - Utilities
+# A: Useful functions
+# Mathematical utilities
+v1 <- c(1.1, -7.1, 5.4, -2.7)
+v2 <- c(-3.6, 4.1, 5.8, -8.0)
+mean(c(sum(round(abs(v1))), sum(round(abs(v2)))))
+
+# abs()  - absolute value
+# round()  - rounds inputs
+# sum()  - sums input array (e.g. vector, matrix)
+# mean()  - calculates arithmetic mean
+
+# Functions for data structures
+li <- list(log = TRUE,
+           ch = "hello",
+           int_vec = sort(rep(seq(8, 2, by = -2),
+                              times = 2)))
+
+# list()  # Creates list
+# log  # Logical
+# ch  # Character
+# seq(start, end, by = increments)  # Generates sequence of numbers
+
+seq(8, 2, by = -2)
+
+# rep(x, times = 2)  # Replicates input 2x times in order
+# rep(x, each = 2)  # Replicates input 2x times by element
+
+# sort()  # Sorts input vector, ascending order
+# sort(x, decreasing = TRUE)  # Reverses order
+
+# str()  # Useful to see contents in datastructure in a concise way
+# is.*()  # Checks type
+# as.*()  # Converts type
+
+is.list(c(1,2,3))
+
+is.list(li)  # Checks whether li is list
+li2 <- as.list(c(1,2,3))
+is.list(li2)
+
+unlist(li)  # Flattens list structure and returns vector
