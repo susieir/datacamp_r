1fd3b11fcf77e9a95842a9433dfea7d07453873d
Susie <susie@irons.email>
2021-01-20 19:07:34 +0000

    Finished intro to tidyverse

--- Introduction_to_the_tidyverse.R
+++ Introduction_to_the_tidyverse.R
@@ -122,3 +122,143 @@ ggplot(gapminder_2007,
   aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
   geom_point() +
   scale_x_log10()
+
+# Section 4 - Faceting
+# Faceting - Dividing plot into subplots
+ggplot(gapminder_2007,
+       aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) +
+  geom_point() +
+  scale_x_log10() +
+  facet_wrap(~ continent)  # Splits plots by continent
+
+# Chapter 3 - Grouping and summarising
+
+# Section 1 - The summarise verb
+# Data transformation and visualisation
+# Summarise() - turns many rows into one
+
+gapminder %>%
+  summarize(meanLifeExp = mean(lifeExp))
+# Collapses to single row
+
+# Summarising one year
+gapminder %>%
+  filter(year == 2007) %>%
+  summarize(meanLifeExp = mean(lifeExp))
+
+# Summarising into multiple columns
+gapminder %>%
+  filter(year == 2007) %>%
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(pop))
+
+# Functions you can use for summarizing
+# -  Mean
+# -  Sum
+# -  Median
+# -  Min
+# -  Max
+
+# Section 2 - The group_by verb
+# group_by() - before summarize() turns groups into one row each
+
+gapminder %>%
+  group_by(year) %>%
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(pop))
+
+# Summarizing by continent
+gapminder %>%
+  filter(year == 2007) %>%
+  group_by(continent) %>%
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(pop))
+
+# Summarizing by continent and year
+gapminder %>%
+  group_by(continent, year) %>%
+  summarize(meanLifeExp = mean(lifeExp),
+            totalPop = sum(pop))
+
+# Section 3 - Visualizing summarised data
+# Summarizing by year
+# Save as object
+by_year <- gapminder %>%
+  group_by(year) %>%
+  summarize(totalPop = sum(pop),
+            meanLifeExp = mean(lifeExp))
+
+by_year
+
+# Visualising population over time
+ggplot(by_year, aes(x = year, y = totalPop)) +
+  geom_point()
+
+# Starting y-axis at zero
+ggplot(by_year, aes(x = year, y = totalPop)) +
+  geom_point() +
+  expand_limits(y = 0)
+
+# Summarising by year and by continent
+by_year_continent <- gapminder %>%
+  group_by(year, continent) %>%
+  summarize(totalPop = sum(pop),
+            meanLifeExp = mean(lifeExp))
+
+by_year_continent
+
+# Visualising by year and by continent
+ggplot(by_year_continent,
+       aes(x = year, y = totalPop, color = continent)) +
+  geom_point() +
+  expand_limits(y = 0)
+
+# Chapter 4 - Types of visualisation
+# Section 1 - Line plots
+# Types of plots
+# Line plots - useful for change over time
+# Bar plots - good at comparing statistics for categories
+# Hists - distribution of 1D numeric variable
+# Box plots - distribution of numeric variable across categories
+
+# Scatter vs. line plot
+# geom_point() - scatter plot
+# geom_line() - line plot
+
+ggplot(by_year_continent,
+       aes(x = year, y = totalPop, color = continent)) +
+  geom_line() +
+  expand_limits(y = 0)
+
+
+# Section 2 - Bar plots - geom_col()
+# Summarizing by continent
+by_continent <- gapminder %>%
+  filter(year == 2007) %>%
+  group_by(continent) %>%
+  summarize(meanLifeExp = mean(lifeExp))
+
+by_continent
+
+ggplot(by_continent,
+       aes(x = continent, y = meanLifeExp)) +
+  geom_col()
+
+# Bar plots always start at 0
+
+# Section 3 - Histograms
+
+# Histogram - geom_histogram()
+# One aesthetic - x axis
+# Width chosen automatically - may need to be customised
+# geom_histogram(bin_width = 5)  # Width of e.g. 5 years
+# You may need to put the x-axis on a log scale
+
+# Section 4 - Box plots
+# Useful if want to compare distribution across categories
+
+ggplot(gapminder_2007,
+      aes(x = continent, y = lifeExp)) +
+  geom_boxplot()
+
+# Gives context to shape of earlier histogram
